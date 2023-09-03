9416467a7ecd471c3d6fd1497fd1be7a85ed596d
Muhammad <muhammadkhizer840@gmail.com>
2023-09-03 18:27:58 +0500

    Fixed phone number bug

--- Backend/model/student.js
+++ Backend/model/student.js
@@ -32,7 +32,7 @@ const studentSchema = new mongoose.Schema({
         required:true
     },
     phoneNo:{
-        type:Number,
+        type:String,
         required:true
     },
     dateOfAdmission:{
